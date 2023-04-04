1f75c866efb5116977625eb36ee7dbc695d092a4
Mike Harris <mharris717@gmail.com>
2023-04-04 10:47:49 -0400

    CS

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,17 @@ export async function possiblyRespond(ops: Ops) {
   }
 }
 
+export async function forceRespond(ops: Ops) {
+  const base = {
+    owner: ops.owner,
+    repo: ops.repo,
+    pull_number: ops.pr,
+    comment_id: ops.comment,
+  }
+  const pr = await GithubPR.get(ops.owner, ops.repo, ops.pr)
+  await respond(pr, ops.comment)
+}
+
 // async function getComments(prNumber: number) {
 //   const comments = await octokit.rest.pulls.listReviewComments({
 //     ...baseParams,
@@ -53,24 +64,23 @@ export async function possiblyRespond(ops: Ops) {
 //   return comments.data
 // }
 
+// https://github.com/hwchase17/langchain/pull/965/files?w=1#r1114829890
+
 async function main() {
   // const comments = await getComments(4)
   // const id = comments.at(-1)!.id
   // const id = 1154549989
   // await respond(4, id)
-
   // https://github.com/hwchase17/langchain/pull/2106
-  const pr = await GithubPR.get('hwchase17', 'langchain', 2106)
-  console.log(pr.baseBranch)
-  console.log(pr.headBranch)
-
-  // return possiblyRespond({
-  //   pr: 4,
-  //   comment: 1155152041,
-  //   owner: 'mharris717',
-  //   repo: 'todo-pr',
+  // const pr = await GithubPR.get('hwchase17', 'langchain', 2106)
+  // console.log(pr.baseBranch)
+  // console.log(pr.headBranch)
+  // return forceRespond({
+  //   pr: 965,
+  //   comment: 1114829890,
+  //   owner: 'hwchase17',
+  //   repo: 'langchain',
   // })
-
   // const foo = await git.show('HEAD:help.py')
   // console.log(foo)
 }
